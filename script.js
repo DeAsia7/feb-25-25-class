42ee90306db359bfc753876c7fd7a9df34e63d16
DeAsia7 <asiamallory277@gmail.com>
2025-02-26 00:10:57 -0500

    update

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ rl2.question("What is your name?", (username) => {
 });
 */
 //exercise 2 favorite color
-
+/*
 const readline = require(`readline`);
 const rl3 = readline.createInterface({
     input: process.stdin,
@@ -43,11 +43,43 @@ rl3.question("What is your name?", (username) => {
 console.log(`Hey, ${username}!`);
 rl3.question("What is your top 3 favorite colors?", (colors) => {
 console.log(`Hey ${username}!, Your top 3 favorite colors are ${colors}.`);
-
         rl3.close();
 });
 });
-
+*/
 
 //exercise 3
 //guessing game. you will ask the user for a number between 1 and 10. user get 3 tries. let user know how many attempots they have left and if their guess is correct or not. user run out of attempts prints you lose .
+
+const readline = require(`readline`);
+const rl4 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+rl4.question("what is your name?", (username) => {
+    console.log(`Hey, ${username}! lets play a game.`);
+rl4.question("Guess a number between 1 and 10?", (guess) => {
+    let X = Math.floor((Math.random() * 10) + 1);
+    let attempts = 3;
+    let correct = false;
+    while (attempts > 0 && !correct) {
+        if (guess == X) {
+            console.log("You win!");
+            correct = true;
+        } else {
+            attempts--;
+            if (attempts > 0) {
+                console.log(`You have ${attempts} attempts left.`);
+                rl4.question("Guess again?", (guess) => {
+                    console.log(`You guessed ${guess}.`);
+                });
+            } else {
+                console.log("You lose!");
+            }
+        }
+    }
+    rl4.close();
+}
+);
+});
+
